81158b46274b453baafa862f13859ad4ab2a0e29
wzw <1519900797@qq.com>
2019-07-30 08:36:36 +0800

    库存实体类添加注释（master）

--- core/src/main/java/com/wzw/entity/Store.java
+++ core/src/main/java/com/wzw/entity/Store.java
@@ -6,17 +6,29 @@ import java.util.Date;
 
 public class Store {
     private Integer medicineId;
-
+    /**
+     * 药品编号（master）
+     */
     private Integer medicineMenuNumber;
-
+    /**
+     * 药品名称（master）
+     */
     private String medicineMenuName;
-
+    /**
+     * 药品最下库存（master）
+     */
     private Integer storeMin;
-
+    /**
+     * 药品最大库存(master)
+     */
     private Integer storeMax;
-
+    /**
+     * 药品数量（master）
+     */
     private Integer amount;
-
+    /**
+     * 剩余过期日期（master）
+     */
     private Integer deadline;
     @JSONField(format = "yyyy-MM-dd")
     private Date productionDate;
